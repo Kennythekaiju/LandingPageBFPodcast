9bb61592f2fb7e88398e1275397a44e512c9edd3
Kennythekaiju <kennethbandera@gmail.com>
2022-07-18 20:56:45 +0000

    Fixed navbar and necessary css

---
+++ src/js/component/NavbarSecond.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../styles/navbarSecond.css";
+    
+export const NavbarSecond = ()=> {
+return(
+
+    <div className="navbarContainer">
+    <div class="navbarMain">
+    <a href="#home">Home</a>
+    <a href="#merch">Merch</a>
+    <a href="#tour">Tour Dates</a>
+    
+  </div>
+  
+  <div class="main">
+    <p>Some text some text some text some text..</p>
+  </div> 
+</div>
+    );
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Navbar from "../component/Navbar.jsx";
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/index.css";
 
 //create your first component
 const Home = () => {
 	return (
 		
-			<>
-			<Navbar/>
-			<div className="badCrew">
+<>
+	<div className="navContained">				
+		<Navbar/>
+	</div>
+	<div className="badCrew">
 				<h2>Meet the Bad Friends</h2>
 				<div
 					className="card mb-3"
@@ -20,7 +21,7 @@ const Home = () => {
 								src="https://pbs.twimg.com/media/FNw_rEWVEAI742d.jpg"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -48,7 +49,7 @@ const Home = () => {
 								src="https://deadline.com/wp-content/uploads/2022/05/Ep108-Bobby-Lee-e1653415207547.jpeg?w=640"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -75,7 +76,7 @@ const Home = () => {
 								src="https://s3.amazonaws.com/bit-photos/large/9027349.jpeg"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -105,7 +106,7 @@ const Home = () => {
 								src="https://m.media-amazon.com/images/M/MV5BMTU0MzAzNTU5NF5BMl5BanBnXkFtZTgwMTg5NDI3OTE@._V1_.jpg"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -133,7 +134,7 @@ const Home = () => {
 								src="https://www.qu.edu/4a718d/contentassets/4e8900ecb35141baabc6f27149aada42/andres-rosende-novo-600x600.jpg"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -161,7 +162,7 @@ const Home = () => {
 								src="https://nyfilmschool.files.wordpress.com/2015/04/george-kimmel-pic.jpg"
 								className="img-fluid rounded-start"
 								alt="..."
-							/>
+								/>
 						</div>
 						<div className="col-md-8">
 							<div className="card-body rounded">
@@ -176,7 +177,7 @@ const Home = () => {
 						</div>
 					</div>
 				</div>
-			</div>
+			</div>	
 			<div
 				style={{
 					display: "flex",
@@ -191,9 +192,9 @@ const Home = () => {
 					<h2 style={{color: "#ffffff", marginTop: "5px"}}>Keep in Touch with the Gang</h2>
 				<img
 					style={{ marginTop: "15px",
-						maxWidth: "600px" }}
+					maxWidth: "600px" }}
 					src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/adce8642-153c-46ae-886f-08a668835ea1/dexqy47-a9b9f2ba-0423-475a-9a63-caba25f7cf8e.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2FkY2U4NjQyLTE1M2MtNDZhZS04ODZmLTA4YTY2ODgzNWVhMVwvZGV4cXk0Ny1hOWI5ZjJiYS0wNDIzLTQ3NWEtOWE2My1jYWJhMjVmN2NmOGUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.gRwQ7jQ83Hr5l-S3paF8AhL_2VMiL-T-RBcWGTYlK6Q"
-				/>
+					/>
 			
 
 				<div className="subBtn">
@@ -214,6 +215,7 @@ const Home = () => {
 							</a>
 					</div>
 				</div>
+				
 			</>
 		
 	);
